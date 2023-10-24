a3e692b7ed9294814d3242f7720b0f7b8932cbb9
MaruthiKo <marurohi@gmail.com>
2023-10-24 15:59:49 +0530

    MLFlow for Experiment tracking initialized

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,4 +1,9 @@
 from pipelines.training_pipeline import train_pipeline
+from zenml.client import Client
+
 
 if __name__ == "__main__":
+    print(Client().active_stack.experiment_tracker.get_tracking_uri())
     train_pipeline(data_path="./data/olist_customers_dataset.csv")
+
+# mlflow ui --backend-store-ui "file:C:\Users\ASUS\AppData\Roaming\zenml\local_stores\1125313a-e0a4-41f2-a598-a7c2cf48cc03\mlruns"
